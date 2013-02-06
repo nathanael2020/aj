bdea9ea6db7f77a69b1d4feab6a1c6ea53abd8b5
Nathanael Miksis <nathanael.miksis@gmail.com>
2013-02-06 15:51:06 -0800

    Added /public/spree/ to .gitignore

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,3 +16,37 @@ set :group, "staff"
 
 #default_run_options[:shell] = '/bin/bash'
 default_run_options[:pty] = true
+
+namespace :deploy do
+  task :setup_config, :roles => :app do
+    run "mkdir -p #{shared_path}/config"
+    run "mkdir -p #{shared_path}/system"
+    run "mkdir -p #{shared_path}/spree"
+    CONFIG_FILES.each do |file|
+      put File.read("config/#{file}.example.yml"), "#{shared_path}/config/#{file}.yml"
+    end
+    puts "Now edit the config files in #{shared_path}"
+  end
+  after "deploy:setup", "deploy:setup_config"
+
+  before "deploy:cold", "deploy:install_bundler"
+  task :install_bundler, :roles => :app do
+    run "type -P bundle &>/dev/null || { gem install bundler --no-ri --no-rdoc; }"
+  end
+
+  task :config do
+    CONFIG_FILES.each do |file|
+      run "cd #{release_path}/config && ln -nfs #{shared_path}/config/#{file}.yml #{release_path}/config/#{file}.yml"
+    end
+  end
+
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+
+  task :update_uploads, :roles => [:app] do
+    run "ln -nfs #{deploy_to}#{shared_dir}/spree #{release_path}/public/spree"
+    run "ln -nfs #{deploy_to}#{shared_dir}/system #{release_path}/public/system"
+  end
+
+end
